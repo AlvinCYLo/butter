38a26f4f736e4b5a17ff04f1b58ddb95ced33b1f
Alvin Lo <p8c1b@ugrad.cs.ubc.ca>
2019-11-16 00:48:41 -0800

    clean up

--- src/api/getActivities.js
+++ src/api/getActivities.js
@@ -2,7 +2,7 @@ import fetch from 'isomorphic-fetch';
 
 export const discoverActivities = async (lat, lon, category, start, end) => {
     try {
-        let response = await fetch(`https://localhost:8080/?lat=${lat}&lon=${lon}&category=${category}&start=${start}&end=${end}`, {
+        let response = await fetch(`https://${process.env.REACT_AP_ACTIVITY_SERVICE_URL}/?lat=${lat}&lon=${lon}&category=${category}&start=${start}&end=${end}`, {
             method: 'get'
         });
 
